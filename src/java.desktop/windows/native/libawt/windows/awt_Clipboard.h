576fb22b8ea42af4ef630ffcdf4f5ea566344d23
Ivan Migalev <ivan.migalev@jetbrains.com>
2024-01-15 19:14:52 +0100

    Revert "JBR-5980: Pasting from clipboard not working reliably in Windows."
    This reverts commit b967f5bc910952ece33ce9f3692fa992f0f9ad68.

--- src/java.desktop/windows/native/libawt/windows/awt_Clipboard.h
+++ src/java.desktop/windows/native/libawt/windows/awt_Clipboard.h
@@ -35,7 +35,7 @@
 
 class AwtClipboard {
 private:
-    static volatile BOOL isGettingOwnership;
+    static BOOL isGettingOwnership;
     static volatile BOOL isClipboardViewerRegistered;
     static volatile jmethodID handleContentsChangedMID;
 
